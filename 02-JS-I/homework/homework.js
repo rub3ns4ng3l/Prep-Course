6db630c0d002ddc0e4c275771dd59e060faff37e
Ruben <rubensgarciaj2815@gmail.com>
2022-08-28 14:42:04 -0500

    cambios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -18,7 +18,7 @@ const nuevaMultiplicacion = 10 * 4 === 40 ;
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === 1;
 
-
+//cambios
 // En los próximos 22 problemas, deberás completar la función.
 // Todo tu código irá dentro de las llaves de la función.
 // Asegúrate que usas "return" cuando la consola te lo pida.
